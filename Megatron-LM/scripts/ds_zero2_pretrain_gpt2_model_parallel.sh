ba5b8187e675aac5c625f7b7223912149e4070d7
Cagri Eryilmaz <ceryilma@amd.com>
2020-11-04 16:01:25 +0000

    adding changes for distributed training

--- Megatron-LM/scripts/ds_zero2_pretrain_gpt2_model_parallel.sh
+++ Megatron-LM/scripts/ds_zero2_pretrain_gpt2_model_parallel.sh
@@ -1,26 +1,29 @@
 #! /bin/bash
 
-# Change for multinode config
-MP_SIZE=4
+MP_SIZE=8
 
-NUM_WORKERS=1
-NUM_GPUS_PER_WORKER=16
+NUM_WORKERS=4
+NUM_GPUS_PER_WORKER=8
+
+NP=32 #Change this later to multiplication of two above.
 
 script_path=$(realpath $0)
 script_dir=$(dirname $script_path)
 
 config_json="$script_dir/ds_zero2_config.json"
+config_json="$script_dir/ds_zero2_config_none.json"
+
 gpt_options=" \
        --model-parallel-size ${MP_SIZE} \
-       --num-layers 24 \
-       --hidden-size 1024 \
+       --num-layers 40 \
+       --hidden-size 1536 \
        --num-attention-heads 16 \
-       --batch-size 8 \
+       --batch-size 32 \
        --seq-length 1024 \
        --max-position-embeddings 1024 \
-       --train-iters 100000 \
+       --train-iters 1000 \
        --resume-dataloader \
-       --train-data webtext \
+       --train-data wikipedia \
        --lazy-loader \
        --tokenizer-type GPT2BPETokenizer \
        --split 949,50,1 \
@@ -30,7 +33,7 @@ gpt_options=" \
        --lr-decay-style cosine \
        --weight-decay 1e-2 \
        --clip-grad 1.0 \
-       --warmup .01 \
+       --warmup .1 \
        --checkpoint-activations \
        --deepspeed-activation-checkpointing \
        --fp16 \
@@ -41,8 +44,10 @@ gpt_options="${gpt_options}
 "
 
 
-run_cmd="deepspeed --num_nodes ${NUM_WORKERS} --num_gpus ${NUM_GPUS_PER_WORKER} pretrain_gpt2.py $@ ${gpt_options}"
+run_cmd="deepspeed --hostfile=$script_dir/../hostfile --num_nodes ${NUM_WORKERS} --num_gpus ${NUM_GPUS_PER_WORKER} $script_dir/../pretrain_gpt2.py $@ ${gpt_options} "
 echo ${run_cmd}
+
 eval ${run_cmd}
+#echo ${distrun}
 
 set +x
